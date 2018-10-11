fcf45c4e240d6010397b406cee8612c9a141d371
estebanquintana6 <a01184855@itesm.mx>
2018-10-10 20:37:12 -0500

    parsed outputs

--- Bayes/parse.py
+++ Bayes/parse.py
@@ -1,10 +1,7 @@
-import json
-
-
-
-probs = []
+inp = []
 querys = []
 nodes = []
+
 
 def parse_nodes(string):
     string = string.replace(" ", "")
@@ -26,40 +23,53 @@ def check_node_exists(elements):
 
 def parse_probabilities(string):
     prob = string.replace(" ","").split('=')
+    input_probs = {}
     if "|" in prob[0]:
         aux = prob[0].split("|")
         if check_node_exists(aux):
-            input_probs = {}
             input_probs["nodes"] = aux
-            input_probs["probability"] = prob[1]
-            probs.append(input_probs)
+            input_probs["prob"] = prob[1]
+            inp.append(input_probs)
     else:
         aux = prob[0]
         aux = aux.replace("+", "").replace("-", "").replace(" ","")
         if aux in nodes:
-            input_probs[prob[0]] = prob[1]
+            input_probs["nodes"] = prob[0]
+            input_probs["prob"] = prob[1]
+            inp.append(input_probs)
 
 
+def parse_output(string):
+    output_probs = {}
+    if "|" in string:
+        aux = string.split("|")
+        if check_node_exists(aux):
+            output_probs["nodes"] = aux
+            output_probs["prob"] = None
+            querys.append(output_probs)
+    else:
+        temp_node = string.replace("+", "").replace("-", "").replace(" ", "")
+        if temp_node in nodes:
+            output_probs["nodes"] = string
+            output_probs["prob"] = None
+            querys.append(output_probs)
 
-
-
-# def dictionary(string):
-#     for n in nodes:
-#         dict = {'Node': node[n]}
-#     print (dict)
 
 input_nodes = input('Enter nodes: ')
 nodes = parse_nodes(input_nodes)
 
 number_of_nodes = int(input('Enter the number of probabilities: '))
+
 for i in range(0, number_of_nodes):
     probability = input('Enter probability: ')
     parse_probabilities(probability)
 
-number_of_queries = int(input('Enter the number of Querys: '))
-for i in range(0, number_of_queries):
-    query = input('Enter query: ')
-    parse_query(query)
+output_number = int(input('Enter number of outputs: '))
+for i in range(0, number_of_nodes):
+    out_prob = input('Enter probability: ')
+    parse_output(out_prob)
 
 
-print(probs)
+
+print(inp)
+print(querys)
