23060c4a376cab84b0ae2b0c3733affc5bd1b80c
J <jav@J-MbA.local>
2018-10-10 20:27:11 -0500

    parse changes

--- Bayes/parse.py
+++ Bayes/parse.py
@@ -1,9 +1,9 @@
 import json
 
 
-input_probs = {}
+probs = []
+querys = []
 nodes = []
-
 
 def parse_nodes(string):
     string = string.replace(" ", "")
@@ -28,7 +28,10 @@ def parse_probabilities(string):
     if "|" in prob[0]:
         aux = prob[0].split("|")
         if check_node_exists(aux):
-            input_probs[prob[0]] = prob[1]
+            input_probs = {}
+            input_probs["nodes"] = aux
+            input_probs["probability"] = prob[1]
+            probs.append(input_probs)
     else:
         aux = prob[0]
         aux = aux.replace("+", "").replace("-", "").replace(" ","")
@@ -36,13 +39,26 @@ def parse_probabilities(string):
             input_probs[prob[0]] = prob[1]
 
 
+
+
+
+# def dictionary(string):
+#     for n in nodes:
+#         dict = {'Node': node[n]}
+#     print (dict)
+
 input_nodes = input('Enter nodes: ')
 nodes = parse_nodes(input_nodes)
 
 number_of_nodes = int(input('Enter the number of probabilities: '))
-
 for i in range(0, number_of_nodes):
     probability = input('Enter probability: ')
     parse_probabilities(probability)
 
-print(json.dumps(input_probs))
+number_of_queries = int(input('Enter the number of Querys: '))
+for i in range(0, number_of_queries):
+    query = input('Enter query: ')
+    parse_query(query)
+
+
+print(probs)
